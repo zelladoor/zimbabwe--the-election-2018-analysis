c5d42be1847f7a177a56f25e5664c77360629826
Tinashe James Manzou <zelladoor@users.noreply.github.com>
2018-08-08 00:07:27 +0200

    Pardon the uploads

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Part of a collaborative effort to make the information more accessible and to investigate the factors leading to the result and the implications thereof',
+    author='Tinashe James Manzou',
+    license='MIT',
+)
